07f5ec3640fb10c65979e706d813fc1d270f2f0b
fengyang <fengyang1@le.com>
2017-09-11 18:05:44 +0800

    create

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# fis3-postpackager-djpost-package
-fis3-postpackager-djpost-package
+# fis-postpackager-relative-cssurl
+
+## 说明
+基于[fis-postpackager-relative-url](https://github.com/lrenc/fis-postpackager-relative-url)修改。
+
+## 功能
+
+ - 自动将css中引用的@import、图片、字体地址转换为相对路径
+ - 生成map.json
---
+++ index.js
@@ -0,0 +1,81 @@
+var path = require('path');
+
+
+function trim(str) {
+  return str.replace(/^\s*/g, '').replace(/\s*$/g, '');
+}
+
+function parsePath(str) {
+  //去除可能存在的引号
+  return str.replace(/[\'|\"]?$/g, '');
+}
+// 获取相对路径
+function getRelativePath(from, to) {
+  to = trim(to);
+  //console.log(path.relative(from, to).replace(/\\/g, '/'));
+  var res = path.relative(from, to).replace(/\\/g, '/');
+  if (res.indexOf('.') !== 0) {
+    res = './' + res;
+  }
+  // console.info('from:%s,to:%s,res:%s',from,to,res)
+  return res;
+}
+
+function parseContent(content, dir) {
+  //console.log(file);
+  var regs = [{
+      patten: /url\([\'|\"]?\s*(\/.*?)[\'|\"]?\s*\)/g,
+      handle: function (origin, path) {
+        return origin.replace(/\(.*\)/g, '(' + path + ')');
+      }
+    },
+    {
+      // 没有小括号的import，必须要有引号和分号
+      patten: /@import\s+[\'|\"]\s*(\/.*?)\s*[\'|\"]\s*(?=;)/g,
+      handle: function (origin, path) {
+        return origin.replace(/[\'|\"]\s*(\/.*)\s*[\'|\"]/g, '"' + path + '"');
+      }
+    }
+  ];
+
+  //var reg = /url\([\'|\"]?\s*(\/.*)[\'|\"]?\s*\)/g;
+  for (var i = 0, l = regs.length; i < l; i++) {
+    var reg = regs[i];
+    content = content.replace(reg.patten, function (m, $1) {
+      //console.log(m, $1);
+      var relativePath = getRelativePath(dir, parsePath($1));
+      // 可能有括号也可能没括号
+      return reg.handle(m, relativePath);
+    });
+  }
+  return content;
+
+}
+
+module.exports = function (ret, conf, settings, opt) {
+  // console.log('post-package-relative-url ')
+  fis.util.map(ret.src, function (subpath, file) {
+    if (file.isCssLike) {
+      var content = file.getContent();
+      var urlPre = path.dirname(file.url);
+      file.setContent(parseContent(content, urlPre));
+    }
+  });
+
+  fis.util.map(ret.pkg, function (subpath, file) {
+    if (file.isCssLike) {
+      var content = file.getContent();
+      var urlPre = path.dirname(file.url);
+      file.setContent(parseContent(content, urlPre));
+    }
+  });
+
+  // 生成map.json
+  var root = fis.project.getProjectPath();
+  var ns = fis.get('namespace');
+  var mapFile = ns ? (ns + '-map.json') : 'map.json';
+  var map = fis.file.wrap(path.join(root, mapFile));
+  map.setContent(JSON.stringify(ret.map, null, map.optimizer ? null : 4));
+  ret.pkg[map.subpath] = map;
+
+};
